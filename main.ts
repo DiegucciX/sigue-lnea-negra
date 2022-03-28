d477c496a8c5a79ad20f19b609fffb009590a881
DiegucciX <98874031+DiegucciX@users.noreply.github.com>
2022-03-28 11:27:05 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,12 @@
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-    } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-    }
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        // 
+        maqueen.motorStop(maqueen.Motors.M1)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    } else {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    }
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-    	
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
-    	
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-    	
+        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
     } else {
-    	
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
     }
 })
